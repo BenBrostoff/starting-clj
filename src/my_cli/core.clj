d4c55141e1c69ca5f15058c5cb1fceb5005ac0eb
Ben Brostoff <ben.brostoff@gmail.com>
2015-03-21 19:05:01 -0400

    1:25 and redirect to my website if unknown link

--- src/my_cli/core.clj
+++ src/my_cli/core.clj
@@ -1,13 +1,19 @@
 (ns my-cli.core
   (:use (compojure handler
         [core :only (GET POST defroutes)]))
-  (:require [ring.util.response :as response] 
+  (:require [net.cgrand.enlive-html :as en]
+            [ring.util.response :as response] 
             [ring.adapter.jetty :as jetty]))
 
 
 ;value does not change once its defined once
 (defonce counter (atom 100))
 (defonce urls (atom {}))
+(def brostoff "http://benbrostoff.github.io/")
+
+(defn get-link
+  [id]
+  (if (= (@urls id) nil) brostoff (@urls id)))
 
 ;could write with some data store
 (defn shorten
@@ -20,22 +26,38 @@
   [request]
   {:status 200
    :body (:with-out-str
-          (println request))})
+           (println request))})
 
 (defn server  
   []
-  (shorten "http://benbrostoff.github.io/")
+  (shorten brostoff)
   (jetty/run-jetty #'app {:port 8080 :join? false}))
 
-(defn homepage
+
+(en/deftemplate homepage
+  (en/xml-resource "homepage.html")
   [request]
-  (str @urls))
+  ;;destructure
+  [:#listing :li] (en/clone-for [[id url] @urls]
+                                [:a] (comp
+                                       ;compose fns together; calls last first
+                                       (en/content (format "%s : %s" id url))
+                                       (en/set-attr :href (str \/ id)))))
+
 
 (defn redirect
   [id]
-  (response/redirect (@urls id)))
+  (response/redirect (get-link id)))
 
-(defroutes app
+(defroutes app*
   (GET "/" request (homepage request))
-  (GET "/:id" [id] (redirect id) ))
+  (POST "/shorten" request
+    ;; clj threading macro
+    ;; equivalent to (:url (:params request))
+    (let [id (shorten (-> request :params :url))]
+      (response/redirect "/")))
+  (GET "/:id" [id] 
+    (redirect id) ))
 
+;;apply middleware
+(def app (compojure.handler/site app*))
